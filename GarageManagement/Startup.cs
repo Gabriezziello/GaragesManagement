59db1d9a09355e6d2d83a5a2a703e8b0999a64c9
Gabriel Petrozziello <gabriezziello@hotmail.com>
2020-06-13 14:00:01 +0100

    Add project files.

---
+++ GarageManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GarageManagement.Startup))]
+namespace GarageManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
